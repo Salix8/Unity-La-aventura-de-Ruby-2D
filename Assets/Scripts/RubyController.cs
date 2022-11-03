964986076bbe9bd2703e9dca14618b422f2d85a4
SaulPachecoTrilles <al427443@uji.es>
2022-11-03 19:33:30 +0100

    We can move Ruby left right up and down

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,10 +18,10 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f *  vertical  * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 30.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 30.0f *  vertical  * Time.deltaTime;
 
+        rigidbody2d.MovePosition(position);
     }
 }
